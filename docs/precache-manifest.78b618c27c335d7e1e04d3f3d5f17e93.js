1c864615370d977432ac456b94b85cc62e1c3210
Mariano Lopez <marianolop22@yahoo.com.ar>
2020-10-05 23:55:28 -0300

    docs para deploy

---
+++ docs/precache-manifest.78b618c27c335d7e1e04d3f3d5f17e93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af64d6e39648b3bf91cb2567120dd55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78c53c2049ab3046090f",
+    "url": "/static/css/main.adc0ce9a.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c53c2049ab3046090f",
+    "url": "/static/js/main.faef0ca6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
